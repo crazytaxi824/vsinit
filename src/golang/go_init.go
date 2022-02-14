e924cc40e52accab14253b94522da657d0899ad0
crazytaxi <crazytaxi.git@outlook.com>
2022-02-14 15:37:55 +1100

    update

--- src/golang/go_init.go
+++ src/golang/go_init.go
@@ -1,4 +1,3 @@
-// copy files: .vscode/settints.json, .vscode/launch.json, .editorconfig, .gitignore, .golangci.yml, src/main.go, src/main_test.go
 // install '.golangci.yml' locally for vim-go.
 
 package golang
