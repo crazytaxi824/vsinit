60c772ebdeffa300f85cc13410f8fb50203c89f6
crazytaxi <crazytaxi.git@outlook.com>
2022-03-23 22:31:56 +1100

    format

--- src/resource/react_proj_files/react_common_fn/grid.tsx
+++ src/resource/react_proj_files/react_common_fn/grid.tsx
@@ -1,293 +1,293 @@
-import React from "react";
-import { Grid, Paper, GridSize } from "@mui/material";
+import React from 'react';
+import { Grid, Paper, GridSize } from '@mui/material';
 
 // 设置 Paper 的 style
 const PaperItem = (props: { content: string }): JSX.Element => {
-	return (
-		<Paper
-			sx={{
-				bgcolor: "primary.main",
-				color: "primary.contrastText",
-				height: "60px",
-				lineHeight: "60px",
-				textAlign: "center",
-			}}
-		>
-			{props.content ? props.content : "xs"}
-		</Paper>
-	);
+  return (
+    <Paper
+      sx={{
+        bgcolor: 'primary.main',
+        color: 'primary.contrastText',
+        height: '60px',
+        lineHeight: '60px',
+        textAlign: 'center',
+      }}
+    >
+      {props.content ? props.content : 'xs'}
+    </Paper>
+  );
 };
 
 // 基本用法
 export function GridTest(): JSX.Element {
-	const xsValues: GridSize[] = [8, 4, 6, 6, 4, 4, 4, 2, 2, 2, 2, 2, 2, 6, 4, 2];
+  const xsValues: GridSize[] = [8, 4, 6, 6, 4, 4, 4, 2, 2, 2, 2, 2, 2, 6, 4, 2];
 
-	const gridItems = xsValues.map((v, i) => {
-		return (
-			// 这里使用 xs={} 来设置 item 占多少个 grid 格子。
-			<Grid item xs={v}>
-				<PaperItem content={"xs=" + v.toString()} />
-			</Grid>
-		);
-	});
+  const gridItems = xsValues.map((v, i) => {
+    return (
+      // 这里使用 xs={} 来设置 item 占多少个 grid 格子
+      <Grid item xs={v}>
+        <PaperItem content={'xs=' + v.toString()} />
+      </Grid>
+    );
+  });
 
-	return (
-		// 外面套一个 div 来约束 Grid
-		<div style={{ margin: "20px", borderStyle: "dashed", borderWidth: "1px" }}>
-			<Grid container spacing={1}>
-				{gridItems}
-			</Grid>
-		</div>
-	);
+  return (
+    // 外面套一个 div 来约束 Grid
+    <div style={{ margin: '20px', borderStyle: 'dashed', borderWidth: '1px' }}>
+      <Grid container spacing={1}>
+        {gridItems}
+      </Grid>
+    </div>
+  );
 }
 
 export function GridAutoLayout(): JSX.Element {
-	const arry = [1, 2, 3, 4, 5];
+  const arry = [1, 2, 3, 4, 5];
 
-	const gridItems = arry.map(() => {
-		return (
-			// 这里使用 xs=true 来自动布局，也可以写作 <Grid item xs={true}>
-			<Grid item xs>
-				<PaperItem content="xs" />
-			</Grid>
-		);
-	});
+  const gridItems = arry.map(() => {
+    return (
+      // 这里使用 xs=true 来自动布局, 也可以写作 <Grid item xs={true}>
+      <Grid item xs>
+        <PaperItem content="xs" />
+      </Grid>
+    );
+  });
 
-	return (
-		// 外面套一个 div 来约束 Grid
-		<div style={{ margin: "20px", borderStyle: "dashed", borderWidth: "1px" }}>
-			<Grid container spacing={1}>
-				{gridItems}
-			</Grid>
-		</div>
-	);
+  return (
+    // 外面套一个 div 来约束 Grid
+    <div style={{ margin: '20px', borderStyle: 'dashed', borderWidth: '1px' }}>
+      <Grid container spacing={1}>
+        {gridItems}
+      </Grid>
+    </div>
+  );
 }
 
 // 多行自动布局
 export function GridAutoLayoutMulti(): JSX.Element {
-	const arry = [1, 2, 3, 4, 5];
+  const arry = [1, 2, 3, 4, 5];
 
-	const gridItems = arry.map(() => {
-		return (
-			<Grid item xs>
-				<PaperItem content="xs" />
-			</Grid>
-		);
-	});
+  const gridItems = arry.map(() => {
+    return (
+      <Grid item xs>
+        <PaperItem content="xs" />
+      </Grid>
+    );
+  });
 
-	const arry2 = [1, 2, 3, 4, 5];
+  const arry2 = [1, 2, 3, 4, 5];
 
-	const gridItems2 = arry2.map(() => {
-		return (
-			// 这里使用 xs=true 来自动布局
-			<Grid item xs>
-				<PaperItem content="xs" />
-			</Grid>
-		);
-	});
+  const gridItems2 = arry2.map(() => {
+    return (
+      // 这里使用 xs=true 来自动布局
+      <Grid item xs>
+        <PaperItem content="xs" />
+      </Grid>
+    );
+  });
 
-	return (
-		<div style={{ margin: "20px", borderStyle: "dashed", borderWidth: "1px" }}>
-			<Grid container spacing={1}>
-				{/* 多行的自动布局需要使用多个 container 嵌套，否则会排列在一行中。 */}
-				{/* xs 是 Grid item 的属性，xs={12} 表示占满一整行。 */}
-				<Grid item container xs={12} spacing={2}>
-					{gridItems}
-				</Grid>
-				<Grid item container xs={12} spacing={4}>
-					{gridItems2}
-				</Grid>
-			</Grid>
-		</div>
-	);
+  return (
+    <div style={{ margin: '20px', borderStyle: 'dashed', borderWidth: '1px' }}>
+      <Grid container spacing={1}>
+        {/* 多行的自动布局需要使用多个 container 嵌套, 否则会排列在一行中 */}
+        {/* xs 是 Grid item 的属性, xs={12} 表示占满一整行 */}
+        <Grid item container xs={12} spacing={2}>
+          {gridItems}
+        </Grid>
+        <Grid item container xs={12} spacing={4}>
+          {gridItems2}
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export function GridAutoLayoutMulti2(): JSX.Element {
-	const arry = [1, 2, 3, 4, 5];
+  const arry = [1, 2, 3, 4, 5];
 
-	const gridItems = arry.map(() => {
-		return (
-			// 这里使用 xs=true 来自动布局
-			<Grid item xs={true}>
-				<PaperItem content="xs" />
-			</Grid>
-		);
-	});
+  const gridItems = arry.map(() => {
+    return (
+      // 这里使用 xs=true 来自动布局
+      <Grid item xs={true}>
+        <PaperItem content="xs" />
+      </Grid>
+    );
+  });
 
-	const arry2 = [1, 2, 3, 4, 5];
+  const arry2 = [1, 2, 3, 4, 5];
 
-	const gridItems2 = arry2.map(() => {
-		return (
-			// 这里使用 xs=true 来自动布局
-			<Grid item xs>
-				<PaperItem content="xs" />
-			</Grid>
-		);
-	});
+  const gridItems2 = arry2.map(() => {
+    return (
+      // 这里使用 xs=true 来自动布局
+      <Grid item xs>
+        <PaperItem content="xs" />
+      </Grid>
+    );
+  });
 
-	return (
-		<div style={{ margin: "20px", borderStyle: "dashed", borderWidth: "1px" }}>
-			<Grid container spacing={1}>
-				{/* 这里不使用 Grid item container */}
-				{gridItems}
-				{gridItems2}
-			</Grid>
-		</div>
-	);
+  return (
+    <div style={{ margin: '20px', borderStyle: 'dashed', borderWidth: '1px' }}>
+      <Grid container spacing={1}>
+        {/* 这里不使用 Grid item container */}
+        {gridItems}
+        {gridItems2}
+      </Grid>
+    </div>
+  );
 }
 
 export function GridItemWidth(): JSX.Element {
-	return (
-		<div style={{ margin: "20px", borderStyle: "dashed", borderWidth: "1px" }}>
-			{/* Grid container 不要设置 columnGap */}
-			<Grid container justifyContent="space-between" spacing={1}>
-				<Grid item width="30%">
-					<PaperItem content="width=30%" />
-				</Grid>
-				<Grid item width="40%">
-					<PaperItem content="width=40%" />
-				</Grid>
-				<Grid item width="30%">
-					<PaperItem content="width=30%" />
-				</Grid>
-				<Grid item width="50%">
-					<PaperItem content="width=50%" />
-				</Grid>
-				<Grid item width="40%">
-					<PaperItem content="width=40%" />
-				</Grid>
-				<Grid item width="10%">
-					<PaperItem content="width=10%" />
-				</Grid>
-			</Grid>
-		</div>
-	);
+  return (
+    <div style={{ margin: '20px', borderStyle: 'dashed', borderWidth: '1px' }}>
+      {/* Grid container 不要设置 columnGap */}
+      <Grid container justifyContent="space-between" spacing={1}>
+        <Grid item width="30%">
+          <PaperItem content="width=30%" />
+        </Grid>
+        <Grid item width="40%">
+          <PaperItem content="width=40%" />
+        </Grid>
+        <Grid item width="30%">
+          <PaperItem content="width=30%" />
+        </Grid>
+        <Grid item width="50%">
+          <PaperItem content="width=50%" />
+        </Grid>
+        <Grid item width="40%">
+          <PaperItem content="width=40%" />
+        </Grid>
+        <Grid item width="10%">
+          <PaperItem content="width=10%" />
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export function GridItemWidth2(): JSX.Element {
-	return (
-		<div
-			style={{
-				minWidth: "720px", // 最小宽度 720 > 140*5
-				margin: "20px",
-				borderStyle: "dashed",
-				borderWidth: "1px",
-			}}
-		>
-			{/* Grid container 不要设置 columnGap */}
-			<Grid container justifyContent="space-between">
-				<Grid item width="140px">
-					<PaperItem content="140px" />
-				</Grid>
-				<Grid item width="140px">
-					<PaperItem content="140px" />
-				</Grid>
-				<Grid item width="140px">
-					<PaperItem content="140px" />
-				</Grid>
-				<Grid item width="140px">
-					<PaperItem content="140px" />
-				</Grid>
-				<Grid item width="140px">
-					<PaperItem content="140px" />
-				</Grid>
-			</Grid>
-		</div>
-	);
+  return (
+    <div
+      style={{
+        minWidth: '720px', // 最小宽度 720 > 140*5
+        margin: '20px',
+        borderStyle: 'dashed',
+        borderWidth: '1px',
+      }}
+    >
+      {/* Grid container 不要设置 columnGap */}
+      <Grid container justifyContent="space-between">
+        <Grid item width="140px">
+          <PaperItem content="140px" />
+        </Grid>
+        <Grid item width="140px">
+          <PaperItem content="140px" />
+        </Grid>
+        <Grid item width="140px">
+          <PaperItem content="140px" />
+        </Grid>
+        <Grid item width="140px">
+          <PaperItem content="140px" />
+        </Grid>
+        <Grid item width="140px">
+          <PaperItem content="140px" />
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export function GridComplexLayout(): JSX.Element {
-	return (
-		<div style={{ margin: "20px", borderStyle: "dashed", borderWidth: "1px" }}>
-			<Grid container rowSpacing={10} columnSpacing={1}>
-				{/* 自动布局 */}
-				<Grid container item xs={12} spacing={3}>
-					<Grid item xs>
-						<PaperItem content="xs" />
-					</Grid>
-					<Grid item xs>
-						<PaperItem content="xs" />
-					</Grid>
-					<Grid item xs>
-						<PaperItem content="xs" />
-					</Grid>
-					<Grid item xs>
-						<PaperItem content="xs" />
-					</Grid>
-					<Grid item xs>
-						<PaperItem content="xs" />
-					</Grid>
-				</Grid>
+  return (
+    <div style={{ margin: '20px', borderStyle: 'dashed', borderWidth: '1px' }}>
+      <Grid container rowSpacing={10} columnSpacing={1}>
+        {/* 自动布局 */}
+        <Grid container item xs={12} spacing={3}>
+          <Grid item xs>
+            <PaperItem content="xs" />
+          </Grid>
+          <Grid item xs>
+            <PaperItem content="xs" />
+          </Grid>
+          <Grid item xs>
+            <PaperItem content="xs" />
+          </Grid>
+          <Grid item xs>
+            <PaperItem content="xs" />
+          </Grid>
+          <Grid item xs>
+            <PaperItem content="xs" />
+          </Grid>
+        </Grid>
 
-				{/* 横向布局 */}
-				<Grid container item xs={8} columnSpacing={3} rowSpacing={1}>
-					<Grid item xs={4}>
-						<PaperItem content="xs" />
-					</Grid>
-					<Grid item xs={4}>
-						<PaperItem content="xs" />
-					</Grid>
-					<Grid item xs={4}>
-						<PaperItem content="xs" />
-					</Grid>
-					<Grid item xs={4}>
-						<PaperItem content="xs" />
-					</Grid>
-					<Grid item xs={4}>
-						<PaperItem content="xs" />
-					</Grid>
-					<Grid item xs={4}>
-						<PaperItem content="xs" />
-					</Grid>
-				</Grid>
+        {/* 横向布局 */}
+        <Grid container item xs={8} columnSpacing={3} rowSpacing={1}>
+          <Grid item xs={4}>
+            <PaperItem content="xs" />
+          </Grid>
+          <Grid item xs={4}>
+            <PaperItem content="xs" />
+          </Grid>
+          <Grid item xs={4}>
+            <PaperItem content="xs" />
+          </Grid>
+          <Grid item xs={4}>
+            <PaperItem content="xs" />
+          </Grid>
+          <Grid item xs={4}>
+            <PaperItem content="xs" />
+          </Grid>
+          <Grid item xs={4}>
+            <PaperItem content="xs" />
+          </Grid>
+        </Grid>
 
-				{/* 纵向布局 */}
-				<Grid container item xs={4} rowSpacing={3}>
-					<Grid item xs={12}>
-						<PaperItem content="xs" />
-					</Grid>
-					<Grid item xs={12}>
-						<PaperItem content="xs" />
-					</Grid>
-					<Grid item xs={12}>
-						<PaperItem content="xs" />
-					</Grid>
-				</Grid>
-			</Grid>
-		</div>
-	);
+        {/* 纵向布局 */}
+        <Grid container item xs={4} rowSpacing={3}>
+          <Grid item xs={12}>
+            <PaperItem content="xs" />
+          </Grid>
+          <Grid item xs={12}>
+            <PaperItem content="xs" />
+          </Grid>
+          <Grid item xs={12}>
+            <PaperItem content="xs" />
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export function GridBreakPoint(): JSX.Element {
-	return (
-		<div
-			style={{
-				width: "300px", // 这里将 grid 宽度定死了
-				margin: "20px",
-				borderStyle: "dashed",
-				borderWidth: "1px",
-			}}
-		>
-			<Grid container rowSpacing={3} columnSpacing={1}>
-				<Grid item xs={6} sm={4} md={3} lg={2}>
-					<PaperItem content="xs={6} sm={4} md={3} lg={2}" />
-				</Grid>
-				<Grid item xs={6} sm={4} md={3} lg={2}>
-					<PaperItem content="xs={6} sm={4} md={3} lg={2}" />
-				</Grid>
-				<Grid item xs={6} sm={4} md={3} lg={2}>
-					<PaperItem content="xs={6} sm={4} md={3} lg={2}" />
-				</Grid>
-				<Grid item xs={6} sm={4} md={3} lg={2}>
-					<PaperItem content="xs={6} sm={4} md={3} lg={2}" />
-				</Grid>
-				<Grid item xs={6} sm={4} md={3} lg={2}>
-					<PaperItem content="xs={6} sm={4} md={3} lg={2}" />
-				</Grid>
-				<Grid item xs={6} sm={4} md={3} lg={2}>
-					<PaperItem content="xs={6} sm={4} md={3} lg={2}" />
-				</Grid>
-			</Grid>
-		</div>
-	);
+  return (
+    <div
+      style={{
+        width: '300px', // 这里将 grid 宽度定死了
+        margin: '20px',
+        borderStyle: 'dashed',
+        borderWidth: '1px',
+      }}
+    >
+      <Grid container rowSpacing={3} columnSpacing={1}>
+        <Grid item xs={6} sm={4} md={3} lg={2}>
+          <PaperItem content="xs={6} sm={4} md={3} lg={2}" />
+        </Grid>
+        <Grid item xs={6} sm={4} md={3} lg={2}>
+          <PaperItem content="xs={6} sm={4} md={3} lg={2}" />
+        </Grid>
+        <Grid item xs={6} sm={4} md={3} lg={2}>
+          <PaperItem content="xs={6} sm={4} md={3} lg={2}" />
+        </Grid>
+        <Grid item xs={6} sm={4} md={3} lg={2}>
+          <PaperItem content="xs={6} sm={4} md={3} lg={2}" />
+        </Grid>
+        <Grid item xs={6} sm={4} md={3} lg={2}>
+          <PaperItem content="xs={6} sm={4} md={3} lg={2}" />
+        </Grid>
+        <Grid item xs={6} sm={4} md={3} lg={2}>
+          <PaperItem content="xs={6} sm={4} md={3} lg={2}" />
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
--- src/resource/react_proj_files/react_common_fn/react_hook_form.tsx
+++ src/resource/react_proj_files/react_common_fn/react_hook_form.tsx
@@ -1,156 +1,175 @@
-import React from "react";
-import { useForm } from "react-hook-form";
-import { FormControl, Input, TextField, InputLabel, FormHelperText, FormLabel, FormGroup, FormControlLabel, InputBase, Button, Checkbox, Switch, RadioGroup, Radio } from "@mui/material";
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import {
+  FormControl,
+  Input,
+  TextField,
+  InputLabel,
+  FormHelperText,
+  FormLabel,
+  FormGroup,
+  FormControlLabel,
+  InputBase,
+  Button,
+  Checkbox,
+  Switch,
+  RadioGroup,
+  Radio,
+} from '@mui/material';
 
 // 定义表单数据
 interface IPerson {
-	firstName: string;
-	lastName: string;
+  firstName: string;
+  lastName: string;
 }
 
 export function Switches(): JSX.Element {
-	const {
-		register, // 注册字段
-		handleSubmit, // 处理 submit 事件
-		watch, // 实时监听数据的改变，底层用了 useState()，每次数据改变会刷新整个组件
-		formState: { errors }, // 处理错误
-		reset, // 重置表单
-	} = useForm<IPerson>();
+  const {
+    register, // 注册字段
+    handleSubmit, // 处理 submit 事件
+    watch, // 实时监听数据的改变, 底层用了 useState(), 每次数据改变会刷新整个组件
+    formState: { errors }, // 处理错误
+    reset, // 重置表单
+  } = useForm<IPerson>();
 
-	function mySubmit(data: IPerson, event?: React.BaseSyntheticEvent): void {
-		console.log(JSON.stringify(data, null, 2));
-		// console.log(event);
-	}
+  function mySubmit(data: IPerson, event?: React.BaseSyntheticEvent): void {
+    console.log(JSON.stringify(data, null, 2));
+    // console.log(event);
+  }
 
-	return (
-		<>
-			<form onSubmit={handleSubmit(mySubmit)}>
-				<FormControl component="fieldset">
-					<FormLabel component="legend">formlabel</FormLabel>
-					{/* register 到 RadioGroup 中 */}
-					<RadioGroup row {...register("firstName")}>
-						<FormControlLabel
-							labelPlacement="top"
-							label="form-control-label"
-							control={<Radio />}
-							value="top" // radio 的 value 放到 FormControlLabel 中
-						/>
-						<FormControlLabel
-							labelPlacement="bottom"
-							label="form-control-label"
-							control={<Radio />}
-							value="bottom" // radio 的 value 放到 FormControlLabel 中
-						/>
-					</RadioGroup>
-				</FormControl>
-				<Button type="submit">submit</Button>
-			</form>
-		</>
-	);
+  return (
+    <>
+      <form onSubmit={handleSubmit(mySubmit)}>
+        <FormControl component="fieldset">
+          <FormLabel component="legend">formlabel</FormLabel>
+          {/* register 到 RadioGroup 中 */}
+          <RadioGroup row {...register('firstName')}>
+            <FormControlLabel
+              labelPlacement="top"
+              label="form-control-label"
+              control={<Radio />}
+              value="top" // radio 的 value 放到 FormControlLabel 中
+            />
+            <FormControlLabel
+              labelPlacement="bottom"
+              label="form-control-label"
+              control={<Radio />}
+              value="bottom" // radio 的 value 放到 FormControlLabel 中
+            />
+          </RadioGroup>
+        </FormControl>
+        <Button type="submit">submit</Button>
+      </form>
+    </>
+  );
 }
 
-// switch 是从 checkbox 衍生出来的，标准 HTML input 中没有 switch 类型
+// switch 是从 checkbox 衍生出来的, 标准 HTML input 中没有 switch 类型
 export function MyTextField(): JSX.Element {
-	const {
-		register, // 注册字段
-		handleSubmit, // 处理 submit 事件
-		watch, // 实时监听数据的改变，底层用了 useState()，每次数据改变会刷新整个组件
-		formState: { errors }, // 处理错误
-		reset, // 重置表单
-	} = useForm<IPerson>();
+  const {
+    register, // 注册字段
+    handleSubmit, // 处理 submit 事件
+    watch, // 实时监听数据的改变, 底层用了 useState(), 每次数据改变会刷新整个组件
+    formState: { errors }, // 处理错误
+    reset, // 重置表单
+  } = useForm<IPerson>();
 
-	function mySubmit(data: IPerson, event?: React.BaseSyntheticEvent): void {
-		console.log(JSON.stringify(data, null, 2));
-		// console.log(event);
-	}
+  function mySubmit(data: IPerson, event?: React.BaseSyntheticEvent): void {
+    console.log(JSON.stringify(data, null, 2));
+    // console.log(event);
+  }
 
-	return (
-		<>
-			<form onSubmit={handleSubmit(mySubmit)}>
-				<FormControl component="fieldset">
-					<FormLabel component="legend">formlabel</FormLabel>
-					{/* FormGroup 可以控制组的元素排列 */}
-					<FormGroup row>
-						<FormControlLabel labelPlacement="top" label="form-control-label" control={<Checkbox value="top" {...register("firstName")} />} />
-						<FormControlLabel labelPlacement="bottom" label="form-control-label" control={<Checkbox value="bot" />} />
-					</FormGroup>
-				</FormControl>
-				<Button type="submit">submit</Button>
-			</form>
-		</>
-	);
+  return (
+    <>
+      <form onSubmit={handleSubmit(mySubmit)}>
+        <FormControl component="fieldset">
+          <FormLabel component="legend">formlabel</FormLabel>
+          {/* FormGroup 可以控制组的元素排列 */}
+          <FormGroup row>
+            <FormControlLabel
+              labelPlacement="top"
+              label="form-control-label"
+              control={<Checkbox value="top" {...register('firstName')} />}
+            />
+            <FormControlLabel labelPlacement="bottom" label="form-control-label" control={<Checkbox value="bot" />} />
+          </FormGroup>
+        </FormControl>
+        <Button type="submit">submit</Button>
+      </form>
+    </>
+  );
 }
 
 export function TestFormControl(): JSX.Element {
-	return (
-		<div style={{ margin: "20px" }}>
-			<FormControl>
-				<FormLabel>FormLabel</FormLabel>
-				<FormGroup row>
-					<FormControlLabel
-						label="top" // label 的名字
-						labelPlacement="top" // label 的位置
-						control={<Checkbox value="top" />} // 需要 label 的组件
-					/>
-					<FormControlLabel
-						label="start" // label 的名字
-						labelPlacement="start" // label 的位置
-						control={<Checkbox value="start" />} // 需要 label 的组件
-					/>
-					<FormControlLabel
-						label="bottom" // label 的名字
-						labelPlacement="bottom" // label 的位置
-						control={<Checkbox value="bottom" />} // 需要 label 的组件
-					/>
-					<FormControlLabel
-						label="end" // label 的名字
-						labelPlacement="end" // label 的位置
-						control={<input value="end" />} // 需要 label 的组件
-					/>
-				</FormGroup>
-			</FormControl>
-		</div>
-	);
+  return (
+    <div style={{ margin: '20px' }}>
+      <FormControl>
+        <FormLabel>FormLabel</FormLabel>
+        <FormGroup row>
+          <FormControlLabel
+            label="top" // label 的名字
+            labelPlacement="top" // label 的位置
+            control={<Checkbox value="top" />} // 需要 label 的组件
+          />
+          <FormControlLabel
+            label="start" // label 的名字
+            labelPlacement="start" // label 的位置
+            control={<Checkbox value="start" />} // 需要 label 的组件
+          />
+          <FormControlLabel
+            label="bottom" // label 的名字
+            labelPlacement="bottom" // label 的位置
+            control={<Checkbox value="bottom" />} // 需要 label 的组件
+          />
+          <FormControlLabel
+            label="end" // label 的名字
+            labelPlacement="end" // label 的位置
+            control={<input value="end" />} // 需要 label 的组件
+          />
+        </FormGroup>
+      </FormControl>
+    </div>
+  );
 }
 
 export function CheckBoxSelect(): JSX.Element {
-	const {
-		register, // 注册字段
-		handleSubmit, // 处理 submit 事件
-		watch, // 实时监听数据的改变，底层用了 useState()，每次数据改变会刷新整个组件
-		formState: { errors }, // 处理错误
-		reset, // 重置表单
-	} = useForm<IPerson>();
+  const {
+    register, // 注册字段
+    handleSubmit, // 处理 submit 事件
+    watch, // 实时监听数据的改变, 底层用了 useState(), 每次数据改变会刷新整个组件
+    formState: { errors }, // 处理错误
+    reset, // 重置表单
+  } = useForm<IPerson>();
 
-	function mySubmit(data: IPerson, event?: React.BaseSyntheticEvent): void {
-		console.log(JSON.stringify(data, null, 2));
-		// console.log(event);
-	}
+  function mySubmit(data: IPerson, event?: React.BaseSyntheticEvent): void {
+    console.log(JSON.stringify(data, null, 2));
+    // console.log(event);
+  }
 
-	return (
-		<>
-			<form onSubmit={handleSubmit(mySubmit)}>
-				<FormControl component="fieldset">
-					<FormLabel component="legend">formlabel</FormLabel>
+  return (
+    <>
+      <form onSubmit={handleSubmit(mySubmit)}>
+        <FormControl component="fieldset">
+          <FormLabel component="legend">formlabel</FormLabel>
 
-					{/* ⭐️ register 到 RadioGroup 中 */}
-					<FormGroup row>
-						<FormControlLabel
-							labelPlacement="top"
-							label="A"
-							control={<Checkbox value="A" {...register("firstName")} />}
-							// ⭐️ radio 的 value 放到 FormControlLabel 中
-						/>
-						<FormControlLabel
-							labelPlacement="bottom"
-							label="B"
-							control={<Checkbox value="B" {...register("firstName")} />}
-							// ⭐️ radio 的 value 放到 FormControlLabel 中
-						/>
-					</FormGroup>
-				</FormControl>
-				<Button type="submit">submit</Button>
-			</form>
-		</>
-	);
+          {/* ⭐️ register 到 RadioGroup 中 */}
+          <FormGroup row>
+            <FormControlLabel
+              labelPlacement="top"
+              label="A"
+              control={<Checkbox value="A" {...register('firstName')} />}
+              // ⭐️ radio 的 value 放到 FormControlLabel 中
+            />
+            <FormControlLabel
+              labelPlacement="bottom"
+              label="B"
+              control={<Checkbox value="B" {...register('firstName')} />}
+              // ⭐️ radio 的 value 放到 FormControlLabel 中
+            />
+          </FormGroup>
+        </FormControl>
+        <Button type="submit">submit</Button>
+      </form>
+    </>
+  );
 }
