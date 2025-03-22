589c7bcda66a4aecf2502cf70769f971c4444808
crazytaxi <crazytaxi.git@outlook.com>
2025-03-22 20:09:58 +1100

    add: py_init

--- src/python/py_init.go
+++ src/python/py_init.go
@@ -1,1 +1,43 @@
 package python
+
+import (
+	"log"
+	"os"
+
+	"local/src/util"
+)
+
+func InitProj() error {
+	flags := util.SetFlags()
+	err := flags.FlagSet.Parse(os.Args[2:])
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	// choose file to print
+	if *flags.Print {
+		err = printSingleFile()
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	// choose file to write
+	if *flags.File {
+		err = writeSingleFile()
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	// write all files
+	err = writeProjectFiles()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
